7bde04dd063a710f3d51415f05779b605edf65f8
Baptiste Langlade <baptouuuu@gmail.com>
2013-10-28 20:40:22 +0100

    added a verification that console var exists in window object before trying to output message

--- vendor/sy/src/Lib/Logger/Handler/Console.js
+++ vendor/sy/src/Lib/Logger/Handler/Console.js
@@ -51,7 +51,7 @@ Sy.Lib.Logger.Handler.Console.prototype = Object.create(Sy.Lib.Logger.Handler.In
     handle: {
         value: function (level, message, data) {
 
-            if (this.isHandling(level)) {
+            if (this.isHandling(level) && 'console' in window) {
 
                 var output = '[' + moment().format('YYYY-M-D hh:mm:ss') + ']',
                     extra = data || [];
