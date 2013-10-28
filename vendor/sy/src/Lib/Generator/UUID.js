31245980f64e2e1947ffd2cb2ef384cea79d1ee8
Baptiste Langlade <baptouuuu@gmail.com>
2013-10-28 19:04:32 +0100

    #3 added dependency for the mediator, unique id generator

---
+++ vendor/sy/src/Lib/Generator/UUID.js
@@ -0,0 +1,27 @@
+namespace('Sy.Lib.Generator');
+
+Sy.Lib.Generator.UUID = function () {
+
+};
+
+Sy.Lib.Generator.UUID.prototype = Object.create(Sy.Lib.Generator.Interface.prototype, {
+
+    s4: {
+        value: function () {
+
+            return Math.floor((1 + Math.random()) * 0x10000)
+                .toString(16)
+                .substring(1);
+
+        }
+    },
+
+    generate: {
+        value: function () {
+
+            return this.s4() + this.s4() + '-' + this.s4() + '-' + this.s4() + '-' + this.s4() + '-' + this.s4() + this.s4() + this.s4();
+
+        }
+    }
+
+});
