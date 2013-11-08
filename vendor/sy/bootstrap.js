c8bfd88756f68148e0eb93095df43483ae219782
Baptiste Langlade <baptouuuu@gmail.com>
2013-11-08 15:57:05 +0100

    #3 prefixed all services by 'sy::' to prevent future conflicts

--- vendor/sy/bootstrap.js
+++ vendor/sy/bootstrap.js
@@ -2,24 +2,24 @@ namespace('Sy');
 
 Sy.service = new Sy.ServiceContainer('sy::core');
 
-Sy.service.set('core::generator::uuid', function () {
+Sy.service.set('sy::core::generator::uuid', function () {
 
     return new Sy.Lib.Generator.UUID();
 
 });
 
-Sy.service.set('core::mediator', function () {
+Sy.service.set('sy::core::mediator', function () {
 
     var m = new Sy.Lib.Mediator();
 
-    m.setGenerator(Sy.service.get('core::generator::uuid'));
-    m.setLogger(Sy.service.get('core::logger'));
+    m.setGenerator(Sy.service.get('sy::core::generator::uuid'));
+    m.setLogger(Sy.service.get('sy::core::logger'));
 
     return m;
 
 });
 
-Sy.service.set('core::logger', function () {
+Sy.service.set('sy::core::logger', function () {
 
     var logger = new Sy.Lib.Logger.CoreLogger('core'),
         info = new Sy.Lib.Logger.Handler.Console(logger.INFO),
