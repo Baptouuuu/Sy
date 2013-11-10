50fc0dc3469438745eecf988bade4b760fa72653
Baptiste Langlade <baptouuuu@gmail.com>
2013-11-10 16:32:59 +0100

    documentation added to ConfigurationInterface + a new method 'has'

--- vendor/sy/src/ConfiguratorInterface.js
+++ vendor/sy/src/ConfiguratorInterface.js
@@ -1,5 +1,12 @@
 namespace('Sy');
 
+/**
+ * Wrapper to set/retrieve key/value pairs
+ *
+ * @package Sy
+ * @interface
+ */
+
 Sy.ConfiguratorInterface = function () {
 
     this.name = '';
@@ -7,6 +14,15 @@ Sy.ConfiguratorInterface = function () {
 };
 
 Sy.ConfiguratorInterface.prototype = Object.create(Object.prototype, {
+
+    /**
+     * Set a new key/value pair
+     *
+     * @param {string} key
+     * @param {mixed} value
+     *
+     * @return {Sy.ConfiguratorInterface}
+     */
 
     set: {
         value: function (key, value) {
@@ -16,13 +32,41 @@ Sy.ConfiguratorInterface.prototype = Object.create(Object.prototype, {
         }
     },
 
+    /**
+     * Return a previously set value through its key
+     *
+     * @param {string} key
+     *
+     * @return {mixed}
+     */
+
     get: {
         value: function (key) {
 
-            return this;
+            return value;
 
         }
     },
+
+    /**
+     * Check if a key is set in the configuration
+     *
+     * @param {string} key
+     *
+     * @return {boolean}
+     */
+
+    has: {
+        value: function (key) {}
+    },
+
+    /**
+     * Set a name for configuration object
+     *
+     * @param {string} name
+     *
+     * @return {Sy.ConfiguratorInterface}
+     */
 
     setName: {
         value: function (name) {
@@ -33,6 +77,12 @@ Sy.ConfiguratorInterface.prototype = Object.create(Object.prototype, {
 
         }
     },
+
+    /**
+     * Return the configuration name
+     *
+     * @return {string}
+     */
 
     getName: {
         value: function () {
