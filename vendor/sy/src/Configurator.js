fbe3a8fda95616c242a6352ff51cfc9f8ba829c9
Baptiste Langlade <baptouuuu@gmail.com>
2013-11-12 18:24:20 +0100

    added Sy.Configuration and 2 dependencies to easily create attributes in deep objects and to retrieve those attributes

---
+++ vendor/sy/src/Configurator.js
@@ -0,0 +1,104 @@
+namespace('Sy');
+
+/**
+ * Wrapper to set/retrieve key/value pairs
+ *
+ * @package Sy
+ * @class
+ * @implements {Sy.ConfiguratorInterface}
+ */
+
+Sy.Configurator = function () {
+
+    this.name = '';
+    this.config = {};
+
+};
+
+Sy.Configurator.prototype = Object.create(Sy.ConfiguratorInterface.prototype, {
+
+    /**
+     * @inheritDoc
+     */
+
+    set: {
+        value: function (key, value) {
+
+            if (key instanceof Object && !value) {
+                this.config = _.extend(this.config, key);
+            } else {
+                objectSetter.call(this.config, key, value);
+            }
+
+            return this;
+
+        }
+    },
+
+    /**
+     * @inheritDoc
+     */
+
+    get: {
+        value: function (key) {
+
+            if (this.has(key)) {
+                value = objectGetter.call(this.config, key);
+            }
+
+            return value;
+
+        }
+    },
+
+    /**
+     * @inheritDoc
+     */
+
+    has: {
+        value: function (key) {
+
+            try {
+
+                objectGetter.call(this.config, key);
+
+                return true;
+
+            } catch (error) {
+
+                if (error instanceof ReferenceError) {
+                    return false;
+                }
+
+            }
+
+        }
+    },
+
+    /**
+     * @inheritDoc
+     */
+
+    setName: {
+        value: function (name) {
+
+            this.name = name;
+
+            return this;
+
+        }
+    },
+
+    /**
+     * @inheritDoc
+     */
+
+    getName: {
+        value: function () {
+
+            return this.name;
+
+        }
+    }
+
+});
--- vendor/sy/src/functions.js
+++ vendor/sy/src/functions.js
@@ -5,6 +5,8 @@
  * You can modify the root object by doing namespace.call(rootObject, nsString).
  *
  * @param {string} ns
+ *
+ * @return {object} Last object created
  */
 
 function namespace (ns) {
@@ -17,13 +19,86 @@ function namespace (ns) {
     } else if (ns instanceof Array && ns.length > 0) {
         namespaces = ns;
     } else {
-        return;
+        return referer;
     }
 
     referer[namespaces[0]] = referer[namespaces[0]] || {};
 
     ns = namespaces.shift();
 
-    namespace.call(referer[ns], namespaces);
+    return namespace.call(referer[ns], namespaces);
+
+}
+
+/**
+ * Set a value into objects, where the chain of object is defined as a dotted string.
+ * The last element of the string represent the attribute.
+ * The root object can be changed by doing objectSetter.call(rootObject, nsString, value).
+ *
+ * @param {string} ns
+ * @param {mixed} value
+ */
+
+function objectSetter (ns, value) {
+
+    var namespaces = '',
+        attr = '',
+        referer = this,
+        idx = ns.lastIndexOf('.');
+
+    if (idx >= 0) {
+
+        attr = ns.substr(idx + 1);
+        namespaces = ns.substr(0, idx);
+
+        referer = namespace.call(referer, namespaces);
+
+    } else {
+
+        attr = ns;
+
+    }
+
+    referer[attr] = value;
+
+}
+
+/**
+ * Retrieve the attribute in objects, where the chain of object is defined as a dotted string.
+ * The last element of the string represent the attribute.
+ * The root object can be changed by doing objectSetter.call(rootObject, nsString, value).
+ *
+ * @param {string} ns
+ *
+ * @return {mixed}
+ */
+
+function objectGetter (ns) {
+
+    var namespaces = null,
+        referer = this;
+
+    if (referer === undefined) {
+
+        throw new ReferenceError('Undefined object');
+
+    } else if (typeof ns === 'string') {
+        namespaces = ns.split('.');
+
+        if (namespaces.length === 1) {
+            return referer[ns];
+        }
+
+    } else if (ns instanceof Array && ns.length > 1) {
+        namespaces = ns;
+    } else if (ns instanceof Array && ns.length === 1) {
+        return referer[ns[0]];
+    } else {
+        return undefined;
+    }
+
+    ns = namespaces.shift();
+
+    return objectGetter.call(referer[ns], namespaces);
 
 }
