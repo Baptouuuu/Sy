23691f4dbd6ac150d9cf94ccf24e671c41589b47
Baptiste Langlade <baptouuuu@gmail.com>
2013-11-12 19:41:26 +0100

    #4 modified definition to match specifications

--- vendor/sy/src/EntityInterface.js
+++ vendor/sy/src/EntityInterface.js
@@ -1,10 +1,26 @@
 namespace('Sy');
 
+/**
+ * Entity interface
+ *
+ * @package Sy
+ * @interface
+ */
+
 Sy.EntityInterface = function () {
 
 };
 
 Sy.EntityInterface.prototype = Object.create(Object.prototype, {
+
+    /**
+     * Set an attribute value to the entity
+     *
+     * @param {string} attr
+     * @param {mixed} value
+     *
+     * @return {Sy.EntityInterface}
+     */
 
     set: {
         value: function (attr, value) {
@@ -14,12 +30,41 @@ Sy.EntityInterface.prototype = Object.create(Object.prototype, {
         }
     },
 
+    /**
+     * Return an attribute value
+     *
+     * @param {string} attr
+     *
+     * @return {mixed}
+     */
+
     get: {
         value: function (attr) {}
     },
 
+    /**
+     * Register an attribute as index or as a connection to another entity
+     *
+     * @param {string} attr
+     * @param {string} entity If set it will link the attribute to another entity, this param must follow this pattern: \w+::\w+
+     *
+     * @return {Sy.EntityInterface}
+     */
+
     register: {
-        value: function (attr) {}
+        value: function (attr, entity) {}
+    },
+
+    /**
+     * Block the available attributes for this entity. Once set, the list won't be mutable.
+     *
+     * @param {Array} attributes
+     *
+     * @return {Sy.EntityInterface}
+     */
+
+    lock: {
+        value: function (attributes) {}
     }
 
 });
