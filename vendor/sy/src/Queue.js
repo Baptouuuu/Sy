0208b6b8d08964f9167b7b08c271a902d1618bf2
Baptiste Langlade <baptouuuu@gmail.com>
2013-12-07 16:03:34 +0100

    #8 added implementation of the Queue interface

---
+++ vendor/sy/src/Queue.js
@@ -0,0 +1,127 @@
+namespace('Sy');
+
+/**
+ * Default implementation of QueueInterface
+ *
+ * @package Sy
+ * @implements {Sy.QueueInterface}
+ * @class
+ */
+
+Sy.Queue = function () {
+
+    this.data = new Sy.Registry();
+    this.states = [];
+
+};
+
+Sy.Queue.prototype = Object.create(Sy.QueueInterface.prototype, {
+
+    /**
+     * @inheritDoc
+     */
+
+    set: {
+        value: function (state, key, value) {
+
+            if (!this.has(state)) {
+
+                var r = new Sy.Registry();
+
+                this.data.set(state, r);
+                this.states.push(state);
+
+            }
+
+            this.data
+                .get(state)
+                .set(key, value);
+
+            return this;
+
+        }
+    },
+
+    /**
+     * @inheritDoc
+     */
+
+    has: {
+        value: function (state, key) {
+
+            if (key === undefined && this.data.has(state)) {
+                return true;
+            }
+
+            if (this.data.has(state) && this.data.get(state).has(key)) {
+                return true;
+            }
+
+            return false;
+
+        }
+    },
+
+    /**
+     * @inheritDoc
+     */
+
+    get: {
+        value: function (state, key) {
+
+            switch (arguments.length) {
+                case 2:
+                    if (this.has(state, key)) {
+                        return this.data.get(state).get(key);
+                    }
+                    break;
+                case 1:
+                    if (this.has(state)) {
+                        return this.data.get(state).get();
+                    }
+                    break;
+                case 0:
+                    var data = {};
+
+                    for (var s in this.states) {
+                        if (this.states.hasOwnProperty(s)) {
+                            data[s] = this.data.get(state).get();
+                        }
+                    }
+
+                    return data;
+            }
+
+            throw new ReferenceError('"' + key + '" does not exist in "' + state + '" state');
+
+        }
+    },
+
+    /**
+     * @inheritDoc
+     */
+
+    state: {
+        value: function (key) {
+
+            var states = [];
+
+            for (var s in this.states) {
+                if (this.data.get(s).has(key)) {
+                    states.push(s);
+                }
+            }
+
+            switch (states.length) {
+                case 0:
+                    return undefined;
+                case 1:
+                    return states[0];
+                default:
+                    return states;
+            }
+
+        }
+    }
+
+});
