7a328ce25e9306be3927d30c4ff777fcaacb03ac
Baptiste Langlade <baptouuuu@gmail.com>
2013-12-07 14:19:22 +0100

    Added an interface for factories so all of them will use the same method name to generate objects

---
+++ vendor/sy/src/FactoryInterface.js
@@ -0,0 +1,22 @@
+namespace('Sy');
+
+/**
+ * Interface to standardize the factories of the framework
+ *
+ * @package Sy
+ * @interface
+ */
+Sy.FactoryInterface = function () {};
+
+Sy.FactoryInterface.prototype = Object.create(Object.prototype, {
+
+    /**
+     * Method to generate a new instance of the object handle by the factory
+     *
+     * @return {mixed}
+     */
+    make: {
+        value: function () {}
+    }
+
+});
