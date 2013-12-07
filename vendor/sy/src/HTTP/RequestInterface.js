a00379c9da670f992eb500dcf97afac38cc109d1
Baptiste Langlade <baptouuuu@gmail.com>
2013-12-07 17:29:22 +0100

    #9 interfaces for requests and responses added

---
+++ vendor/sy/src/HTTP/RequestInterface.js
@@ -0,0 +1,110 @@
+namespace('Sy.HTTP');
+
+/**
+ * Interface for building HTTP requests
+ *
+ * @package Sy
+ * @subpackage HTTP
+ * @interface
+ */
+
+Sy.HTTP.RequestInterface = function () {};
+
+Sy.HTTP.RequestInterface.prototype = Object.create(Object.prototype, {
+
+    /**
+     * Set the method for the request (GET, POST, ...)
+     *
+     * @param {string} method
+     *
+     * @return {Sy.HTTP.RequestInterface}
+     */
+
+    setMethod: {
+        value: function (method) {}
+    },
+
+    /**
+     * Get the method of the request
+     *
+     * @return {string}
+     */
+
+    getMethod: {
+        value: function () {}
+    },
+
+    /**
+     * Set the data to send in the request
+     *
+     * @param {object} data
+     *
+     * @return {Sy.HTTP.RequestInterface}
+     */
+
+    setData: {
+        value: function (data) {}
+    },
+
+    /**
+     * Get the request data
+     *
+     * @return {object}
+     */
+
+    getData: {
+        value: function () {}
+    },
+
+    /**
+     * Set a header value
+     *
+     * Multiple can be set at a time by passing an object like {headerName: value, ...}
+     *
+     * @param {string|object} header
+     * @param {string} value
+     *
+     * @return {Sy.HTTP.RequestInterface}
+     */
+
+    setHeader: {
+        value: function (header, value) {}
+    },
+
+    /**
+     * Get the value of a header field
+     *
+     * If no header set, it return all of them
+     *
+     * @param {string} header
+     *
+     * @return {string|object}
+     */
+
+    getHeader: {
+        value: function (header) {}
+    },
+
+    /**
+     * Set a listener to the request to be executed when the response is received
+     *
+     * @param {function} fn
+     *
+     * @return {Sy.HHTP.RequestInterface}
+     */
+
+    setListener: {
+        value: function (fn) {}
+    },
+
+    /**
+     * Get the listener
+     *
+     * @return {function}
+     */
+
+    getListener: {
+        value: function () {}
+    }
+
+});
