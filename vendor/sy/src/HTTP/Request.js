a4a034432d8af0c1f1489c54eb663be3b23a8089
Baptiste Langlade <baptouuuu@gmail.com>
2013-12-07 17:56:08 +0100

    #9 added default implementation of the RequestInterface

---
+++ vendor/sy/src/HTTP/Request.js
@@ -0,0 +1,162 @@
+namespace('Sy.HTTP');
+
+/**
+ * Interface for building HTTP requests
+ *
+ * @package Sy
+ * @subpackage HTTP
+ * @implements {Sy.HTTP.RequestInterface}
+ * @class
+ */
+
+Sy.HTTP.Request = function () {
+
+    this.method = 'GET';
+    this.data = new Sy.Registry();
+    this.headers = new Sy.Registry();
+    this.listener = {
+        fn: null,
+        context: window
+    };
+
+};
+
+Sy.HTTP.Request.prototype = Object.create(Sy.HTTP.RequestInterface.prototype, {
+
+    /**
+     * @inheritDoc
+     */
+
+    setMethod: {
+        value: function (method) {
+
+            var m = method.toUpperCase();
+
+            if (['OPTIONS', 'GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'TRACE', 'CONNECT'].indexOf(m) !== -1) {
+
+                this.method = m;
+
+            }
+
+            return this;
+
+        }
+    },
+
+    /**
+     * @inheritDoc
+     */
+
+    getMethod: {
+        value: function () {
+
+            return this.method;
+
+        }
+    },
+
+    /**
+     * @inheritDoc
+     */
+
+    setData: {
+        value: function (data) {
+
+            for (var k in data) {
+                if (data.hasOwnProperty(k)) {
+                    this.data.set(k, data[k]);
+                }
+            }
+
+            return this;
+
+        }
+    },
+
+    /**
+     * @inheritDoc
+     */
+
+    getData: {
+        value: function () {
+
+            return this.data.get();
+
+        }
+    },
+
+    /**
+     * @inheritDoc
+     */
+
+    setHeader: {
+        value: function (header, value) {
+
+            if (header instanceof Object) {
+                for (var k in header) {
+                    if (header.hasOwnProperty(k)) {
+                        this.setHeader(k, header[k]);
+                    }
+                }
+            } else if (typeof header === 'string' && typeof value === 'string') {
+
+                this.headers.set(header, value);
+
+            }
+
+            return this;
+
+        }
+    },
+
+    /**
+     * @inheritDoc
+     */
+
+    getHeader: {
+        value: function (header) {
+
+            if (header !== undefined) {
+                if (this.headers.has(header)) {
+                    return this.headers.get(header);
+                }
+
+                return undefined;
+            }
+
+            return this.headers.getMapping();
+
+        }
+    },
+
+    /**
+     * @inheritDoc
+     */
+
+    setListener: {
+        value: function (fn, context) {
+
+            this.listener.fn = fn;
+
+            if (context !== undefined) {
+                this.listener.context = context;
+            }
+
+            return this;
+
+        }
+    },
+
+    /**
+     * @inheritDoc
+     */
+
+    getListener: {
+        value: function () {
+
+            return this.listener;
+
+        }
+    }
+
+});
