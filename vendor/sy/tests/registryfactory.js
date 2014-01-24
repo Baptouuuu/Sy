53f5d1728645623f96035d436c69f44e2534f602
Baptiste Langlade <baptouuuu@gmail.com>
2014-01-24 20:11:16 +0100

    registry factory UT added

---
+++ vendor/sy/tests/registryfactory.js
@@ -0,0 +1,22 @@
+/**
+ * @venus-library jasmine
+ * @venus-include ../src/functions.js
+ * @venus-include ../src/FactoryInterface.js
+ * @venus-include ../src/RegistryInterface.js
+ * @venus-include ../src/Registry.js
+ * @venus-include ../src/RegistryFactory.js
+ */
+
+describe('registry factory', function () {
+
+    var factory = new Sy.RegistryFactory();
+
+    it('should return a registry', function () {
+
+        var r = factory.make();
+
+        expect(r instanceof Sy.Registry).toBe(true);
+
+    });
+
+});
