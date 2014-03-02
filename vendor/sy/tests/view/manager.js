86cb5bb29399c126a34f2c884e29847fcce00a20
Baptiste Langlade <baptouuuu@gmail.com>
2014-03-02 16:57:04 +0100

    #12 manager UT updated

--- vendor/sy/tests/view/manager.js
+++ vendor/sy/tests/view/manager.js
@@ -1,14 +1,35 @@
 /**
  * @venus-library jasmine
  * @venus-include ../../src/functions.js
- * @venus-include ../../src/View/Manager.js
+ * @venus-include ../../src/FactoryInterface.js
+ * @venus-include ../../src/RegistryInterface.js
+ * @venus-include ../../src/Registry.js
+ * @venus-include ../../src/RegistryFactory.js
+ * @venus-include ../../src/View/TemplateEngineInterface.js
+ * @venus-include ../../src/View/Parser.js
  * @venus-include ../../src/View/NodeWrapper.js
+ * @venus-include ../../src/View/Layout.js
+ * @venus-include ../../src/View/LayoutFactoryInterface.js
+ * @venus-include ../../src/View/LayoutFactory.js
  * @venus-include ../../src/View/ViewScreen.js
+ * @venus-include ../../src/View/ViewScreenFactoryInterface.js
+ * @venus-include ../../src/View/ViewScreenFactory.js
+ * @venus-include ../../src/View/Manager.js
  */
 
 describe('view manager', function () {
 
-    var manager = new Sy.View.Manager();
+    var manager = new Sy.View.Manager(),
+        viewscreenFactory = new Sy.View.ViewScreenFactory(),
+        layoutFactory = new Sy.View.LayoutFactory();
+
+    viewscreenFactory.setParser(new Sy.View.Parser());
+    viewscreenFactory.setTemplateEngine(new Sy.View.TemplateEngineInterface());
+    viewscreenFactory.setRegistryFactory(new Sy.RegistryFactory());
+    viewscreenFactory.setLayoutFactory(layoutFactory);
+
+    manager.setViewsRegistry(new Sy.Registry());
+    manager.setViewScreenFactory(viewscreenFactory);
 
     it('should register a new view screen', function () {
 
