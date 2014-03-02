4f24cf6c9209e58c68a704b617633a6a74198957
Baptiste Langlade <baptouuuu@gmail.com>
2014-03-02 16:16:10 +0100

    #12 updated the viewscreen UT

--- vendor/sy/tests/view/viewscreen.js
+++ vendor/sy/tests/view/viewscreen.js
@@ -1,47 +1,46 @@
 /**
  * @venus-library jasmine
  * @venus-include ../../src/functions.js
+ * @venus-include ../../src/FactoryInterface.js
+ * @venus-include ../../src/RegistryInterface.js
+ * @venus-include ../../src/Registry.js
+ * @venus-include ../../src/RegistryFactory.js
+ * @venus-include ../../src/View/TemplateEngineInterface.js
  * @venus-include ../../src/View/NodeWrapper.js
+ * @venus-include ../../src/View/Parser.js
+ * @venus-include ../../src/View/Layout.js
+ * @venus-include ../../src/View/ListFactoryInterface.js
+ * @venus-include ../../src/View/LayoutFactoryInterface.js
+ * @venus-include ../../src/View/LayoutFactory.js
  * @venus-include ../../src/View/ViewScreen.js
+ * @venus-fixture ../../fixtures/view/viewscreen.html
  */
 
 describe('viewscreen', function () {
 
     var viewscreen = new Sy.View.ViewScreen(),
-        node = document.createElement('section'),
-        MockEngine = function () {},
-        val;
+        factory = new Sy.View.LayoutFactory(),
+        node = document.querySelector('[data-sy-view]');
 
-    MockEngine.prototype = Object.create(Sy.View.TemplateEngineInterface.prototype, {
-        render: {
-            value: function () {
-                val = 'rendered';
-            }
-        }
-    });
+    factory.setTemplateEngine(new Sy.View.TemplateEngineInterface());
+    factory.setParser(new Sy.View.Parser());
+    factory.setRegistryFactory(new Sy.RegistryFactory());
+    factory.setListFactory(new Sy.View.ListFactoryInterface());
 
-    node.dataset.syViewScreen = 'name';
+    viewscreen.setParser(new Sy.View.Parser());
+    viewscreen.setLayoutsRegistry(new Sy.Registry());
+    viewscreen.setLayoutFactory(factory);
     viewscreen.setNode(node);
 
     it('should return the name', function () {
 
-        expect(viewscreen.getName()).toEqual('name');
-
-    });
-
-    it('should render the node', function () {
-
-        viewscreen.setTemplateEngine(new MockEngine());
-        viewscreen.render({some: 'data'});
-
-        expect(val).toEqual('rendered');
+        expect(viewscreen.getName()).toEqual('viewScreenName');
 
     });
 
     it('should return the list of layouts', function () {
-
         expect(viewscreen.getLayouts() instanceof Array).toBe(true);
-        expect(viewscreen.getLayouts().length).toEqual(0);
+        expect(viewscreen.getLayouts().length).toEqual(1);
 
     });
 
