4105926de8a4b2d08d5458d670952a9e19f4b689
Baptiste Langlade <baptouuuu@gmail.com>
2014-03-16 17:53:51 +0100

    in the kernel load metadata in the configurator + register app services

--- src/Kernel/Core.js
+++ src/Kernel/Core.js
@@ -36,6 +36,62 @@ Sy.Kernel.Core.prototype = Object.create(Object.prototype, {
         value: function () {
             return this.container;
         }
+    },
+
+    /**
+     * Initiate the kernel that will inspect the app and build necessary data
+     *
+     * @return {Sy.Kernel.Core}
+     */
+
+    init: {
+        value: function () {
+
+            var parser = new Sy.Kernel.AppParser();
+
+            this.config.set('parameters.app.meta', {
+                bundles: parser.getBundles(),
+                controllers: parser.getControllers(),
+                entities: parser.getEntities(),
+                viewscreens: parser.getViewScreens()
+            });
+
+            this.registerServices(parser.getServices());
+
+        }
+    },
+
+    /**
+     * Register the app services in the global container
+     *
+     * @param {Array} services
+     *
+     * @return {Sy.Kernel.Core}
+     */
+
+    registerServices: {
+        value: function (services) {
+
+            for (var i = 0, l = services.length; i < l; i++) {
+                if (services[i].creator) {
+                    this.container.set(
+                        services[i].name,
+                        services[i].creator
+                    );
+                } else if (typeof services[i].constructor === 'string') {
+                    var name = services[i].name;
+                    delete services[i].name;
+
+                    this.container.set(
+                        name,
+                        services[i]
+                    );
+                }
+            }
+
+            return this;
+
+        }
     }
 
 });
