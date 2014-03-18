a2e0b36a629fca141a1b0d312e3cee5f66ebfd21
Baptiste Langlade <baptouuuu@gmail.com>
2014-03-18 21:13:31 +0100

    #20 engine factories added

---
+++ src/Storage/EngineFactory/IndexedDBFactory.js
@@ -0,0 +1,70 @@
+namespace('Sy.Storage.EngineFactory');
+
+/**
+ * Generates an instance of an indexedDB storage engine
+ *
+ * @package Sy
+ * @subpackage Storage.EngineFactory
+ * @class
+ * @extends {Sy.Storage.EngineFactory.AbstractFactory}
+ * @implements {Sy.FactoryInterface}
+ */
+
+Sy.Storage.EngineFactory.IndexedDBFactory = function () {
+    Sy.Storage.EngineFactory.AbstractFactory.call(this);
+};
+Sy.Storage.EngineFactory.IndexedDBFactory.prototype = Object.create(Sy.Storage.EngineFactory.AbstractFactory.prototype, {
+
+    /**
+     * @inheritDoc
+     */
+
+    make: {
+        value: function (name, version, stores) {
+
+            name = name || 'app::storage';
+            version = version || 1;
+            stores = stores || [];
+
+            var engine = new Sy.Storage.Engine.IndexedDB(version);
+
+            for (var i = 0, l = stores.length; i < l; i++) {
+                engine.setStore(
+                    stores[i].alias,
+                    stores[i].name,
+                    stores[i].indetifier,
+                    stores[i].indexes
+                );
+            }
+
+            if (this.logger) {
+                engine.setLogger(this.logger);
+            }
+
+            if (this.mediator) {
+                engine.setMediator(this.mediator);
+            }
+
+            engine
+                .setConnection(
+                    window.indexedDB ||
+                    window.webkitIndexedDB ||
+                    window.mozIndexedDB ||
+                    window.msIndexedDB
+                )
+                .setTransaction(
+                    window.IDBTransaction ||
+                    window.webkitIDBTransaction
+                )
+                .setKeyRange(
+                    window.IDBKeyRange ||
+                    window.webkitIDBKeyRange
+                )
+                .open();
+
+            return engine;
+
+        }
+    }
+
+});
