19c2c5733656069e22b732586676480b187f468b
Baptiste Langlade <baptouuuu@gmail.com>
2014-04-09 20:43:59 +0200

    added method to add headers to the http storage requests

--- src/Storage/Engine/Rest.js
+++ src/Storage/Engine/Rest.js
@@ -16,6 +16,7 @@ Sy.Storage.Engine.Rest = function (version) {
     this.manager = null;
     this.basePath = '';
     this.mediator = null;
+    this.headers = {};
 
 };
 
@@ -86,6 +87,23 @@ Sy.Storage.Engine.Rest.prototype = Object.create(Sy.Storage.EngineInterface.prot
     },
 
     /**
+     * Set the headers that will be associated to every request made by the engine
+     * Useful to set authentication tokens
+     *
+     * @param {Object} headers
+     *
+     * @return {Sy.Storage.Engine.Rest}
+     */
+
+    setHeaders: {
+        value: function (headers) {
+            this.headers = headers;
+
+            return this;
+        }
+    },
+
+    /**
      * @inheritDoc
      */
 
@@ -120,6 +138,7 @@ Sy.Storage.Engine.Rest.prototype = Object.create(Sy.Storage.EngineInterface.prot
                 uri: this.basePath
                     .replace(/{{path}}/, meta.path)
                     .replace(/{{key}}/, identifier),
+                headers: this.headers,
                 listener: function (resp) {
 
                     callback(resp.getBody());
@@ -156,6 +175,7 @@ Sy.Storage.Engine.Rest.prototype = Object.create(Sy.Storage.EngineInterface.prot
                 uri: this.basePath
                     .replace(/{{path}}/, meta.path)
                     .replace(/{{key}}/, ''),
+                headers: this.headers,
                 data: object,
                 listener: function (resp) {
 
@@ -201,6 +221,7 @@ Sy.Storage.Engine.Rest.prototype = Object.create(Sy.Storage.EngineInterface.prot
                 uri: this.basePath
                     .replace(/{{path}}/, meta.path)
                     .replace(/{{key}}/, identifier),
+                headers: this.headers,
                 data: object,
                 listener: function (resp) {
 
@@ -246,6 +267,7 @@ Sy.Storage.Engine.Rest.prototype = Object.create(Sy.Storage.EngineInterface.prot
                 uri: this.basePath
                     .replace(/{{path}}/, meta.path)
                     .replace(/{{key}}/, identifier),
+                headers: this.headers,
                 listener: function (resp) {
 
                     callback(identifier);
@@ -293,6 +315,7 @@ Sy.Storage.Engine.Rest.prototype = Object.create(Sy.Storage.EngineInterface.prot
                 uri: this.basePath
                     .replace(/{{path}}/, meta.path)
                     .replace(/{{key}}/, '?' + queries.join('&')),
+                headers: this.headers,
                 listener: function (resp) {
 
                     callback(resp.getBody());
