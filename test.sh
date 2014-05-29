6ebd0e58ee16dbc7460890434b55ed0205a4a23d
Baptiste Langlade <baptouuuu@gmail.com>
2014-05-29 18:42:13 +0200

    fix test runner, chain commands so if one fails the whole stops

--- test.sh
+++ test.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-grunt jscs
-if [[ $? == "1" ]] ; then
-    exit 1;
-fi;
-./node_modules/venus/bin/venus run -t tests/ -c -n --singleton
+grunt jscs && ./node_modules/venus/bin/venus run -t tests/ -c -n --singleton
