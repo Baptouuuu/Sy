7af0535cf95e2e3a089d1a93c1549823037c3195
Baptiste Langlade <baptouuuu@gmail.com>
2014-06-09 16:22:05 +0200

    added possiblity to create forms via form types

--- src/Form/Builder.js
+++ src/Form/Builder.js
@@ -10,6 +10,7 @@ namespace('Sy.Form');
 
 Sy.Form.Builder = function () {
     this.validator = null;
+    this.types = {};
 };
 Sy.Form.Builder.prototype = Object.create(Object.prototype, {
 
@@ -60,6 +61,82 @@ Sy.Form.Builder.prototype = Object.create(Object.prototype, {
             }
 
             return builder;
+        }
+    },
+
+    /**
+     * Register a new form type
+     *
+     * @param {Sy.Form.FormTypeInterface} formType
+     *
+     * @return {Sy.Form.Builder} self
+     */
+
+    registerFormType: {
+        value: function (formType) {
+            if (!(formType instanceof Sy.Form.FormTypeInterface)) {
+                throw new TypeError('Invalid form type');
+            }
+
+            this.types[formType.getName()] = formType;
+
+            return this;
+        }
+    },
+
+    /**
+     * Create a form based on the form type
+     *
+     * @param {Sy.Form.FormTypeInterface|String} formType
+     * @param {Object} options
+     *
+     * @return {Sy.Form.FormInterface}
+     */
+
+    createForm: {
+        value: function (formType, options) {
+            if (typeof formType === 'string') {
+                if (!this.types[formType]) {
+                    throw new ReferenceError('Form type "' + formType + '" is undefined');
+                }
+
+                formType = this.types[formType];
+            }
+
+            var builder = new Sy.Form.FormBuilder(),
+                config = new Sy.Configurator(),
+                dataClass;
+
+            builder.setName(formType.getName());
+            formType.setDefaultOptions(config);
+
+            if (options) {
+                for (var prop in options) {
+                    if (options.hasOwnProperty(prop)) {
+                        config.set(prop, options[prop]);
+                    }
+                }
+            }
+
+            formType.buildForm(builder, config);
+
+            builder.setOptions(config);
+
+            if (config.has('dataClass')) {
+                dataClass = objectGetter(config.get('dataClass'));
+
+                if (!dataClass) {
+                    throw new ReferenceError('Data class "' + config.get('dataClass') +'" is undefined');
+                }
+
+                builder.setObject(new dataClass());
+            }
+
+            if (this.validator) {
+                builder.setValidator(this.validator);
+            }
+
+            return builder.getForm();
         }
     }
 
---
+++ tests/form/builder.js
@@ -0,0 +1,121 @@
+/**
+ * @venus-library jasmine
+ * @venus-include ../../vendor/underscore/underscore.js
+ * @venus-include ../../src/functions.js
+ * @venus-include ../../src/ConfiguratorInterface.js
+ * @venus-include ../../src/Configurator.js
+ * @venus-include ../../src/Validator/Core.js
+ * @venus-include ../../src/Form/FormInterface.js
+ * @venus-include ../../src/Form/Form.js
+ * @venus-include ../../src/Form/FormBuilderInterface.js
+ * @venus-include ../../src/Form/FormBuilder.js
+ * @venus-include ../../src/Form/FormTypeInterface.js
+ * @venus-code ../../src/Form/Builder.js
+ */
+
+describe('form builder', function () {
+
+    var abstractType = function () {},
+        builder;
+
+    window.User = function () {};
+
+    abstractType.prototype = Object.create(Sy.Form.FormTypeInterface.prototype, {
+        buildForm: {
+            value: function (builder, options) {
+                builder.add('foo');
+            }
+        },
+        setDefaultOptions: {
+            value: function (options) {
+                options.set({
+                    dataClass: 'User'
+                });
+            }
+        },
+        getName: {
+            value: function () {
+                return 'typeName';
+            }
+        }
+    });
+
+    beforeEach(function () {
+        builder = new Sy.Form.Builder();
+        builder.setValidator(new Sy.Validator.Core());
+    });
+
+    it('should throw if trying to set invalid validator', function () {
+        expect(function () {
+            builder.setValidator({});
+        }).toThrow('Invalid validator');
+    });
+
+    it('should set the validator', function () {
+        expect(builder.setValidator(new Sy.Validator.Core())).toEqual(builder);
+        expect(builder.validator instanceof Sy.Validator.Core).toBe(true);
+    });
+
+    it('should create a form builder', function () {
+        var object = {foo: 'bar'},
+            formBuilder = builder.createFormBuilder(object, {optionKey: 'value'});
+
+        expect(formBuilder instanceof Sy.Form.FormBuilderInterface).toBe(true);
+        expect(formBuilder.getForm().getObject()).toEqual(object);
+        expect(formBuilder.getForm().config.has('optionKey')).toBe(true);
+    });
+
+    it('should throw if trying to set invalid form type', function () {
+        expect(function () {
+            builder.registerFormType({});
+        }).toThrow('Invalid form type');
+    });
+
+    it('should register a form type', function () {
+        expect(builder.registerFormType(new abstractType())).toEqual(builder);
+        expect(builder.types.typeName instanceof abstractType).toBe(true);
+    });
+
+    it('should throw if trying to create form from unknown form type', function () {
+        expect(function () {
+            builder.createForm('unknown');
+        }).toThrow('Form type "unknown" is undefined');
+    });
+
+    it('should create a form from a registered form type', function () {
+        builder.registerFormType(new abstractType());
+
+        var form = builder.createForm('typeName', {
+            someKey: 'value'
+        });
+
+        expect(form instanceof Sy.Form.FormInterface).toBe(true);
+        expect(form.config.has('someKey')).toBe(true);
+        expect(form.config.has('dataClass')).toBe(true);
+        expect(form.config.get('dataClass')).toEqual('User');
+        expect(form.elements.length).toEqual(1);
+        expect(form.getObject() instanceof User).toBe(true);
+    });
+
+    it('should create a form from the form type instance', function () {
+        var form = builder.createForm(new abstractType(), {
+            someKey: 'value'
+        });
+
+        expect(form instanceof Sy.Form.FormInterface).toBe(true);
+        expect(form.config.has('someKey')).toBe(true);
+        expect(form.config.has('dataClass')).toBe(true);
+        expect(form.config.get('dataClass')).toEqual('User');
+        expect(form.elements.length).toEqual(1);
+        expect(form.getObject() instanceof User).toBe(true);
+    });
+
+    it('should throw if data class is undefined', function () {
+        expect(function () {
+            builder.createForm(new abstractType(), {
+                dataClass: 'Unknown'
+            });
+        }).toThrow('Data class "Unknown" is undefined');
+    });
+
+});
