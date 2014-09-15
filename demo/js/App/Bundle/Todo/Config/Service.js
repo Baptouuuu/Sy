0322e85ef29a915e36df77a44ca45b26f18a53b2
Baptiste Langlade <baptouuuu@gmail.com>
2014-09-15 13:18:55 +0200

    made the repo listener compliant with the latest event subscriber

--- demo/js/App/Bundle/Todo/Config/Service.js
+++ demo/js/App/Bundle/Todo/Config/Service.js
@@ -11,8 +11,10 @@ App.Bundle.Todo.Config.Service.prototype = Object.create(Object.prototype, {
                     constructor: 'App.Bundle.Todo.Service.TaskRepoListener',
                     calls: [
                         ['setRest', ['@sy::core::http::rest']],
-                        ['setMediator', ['@sy::core::mediator']],
                         ['setApiPath', ['%api.basePath%']]
+                    ],
+                    tags: [
+                        {name: 'event.subscriber'}
                     ]
                 },
                 'todo::viewscreen::main': {
