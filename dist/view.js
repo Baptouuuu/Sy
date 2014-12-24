9d88d3025a293f9059ea7225f8baa64430903bf4
Baptiste Langlade <baptouuuu@gmail.com>
2014-12-24 13:06:00 +0100

    update version to 1.1.1

--- dist/view.js
+++ dist/view.js
@@ -1,4 +1,4 @@
-/*! sy#1.1.0 - 2014-12-20 */
+/*! sy#1.1.1 - 2014-12-24 */
 /**
  * Transform a dotted string to a multi level object.
  * String like "Foo.Bar.Baz" is like doing window.Foo = {Bar: {Baz: {}}}.
@@ -841,7 +841,7 @@ Sy.EventDispatcher.EventDispatcher.prototype = Object.create(Sy.EventDispatcher.
                     } else {
                         this.addListener(
                             name,
-                            subscriber[subscribed[name]]
+                            [subscriber, subscriber[subscribed[name]]]
                         );
                     }
                 }
@@ -923,7 +923,7 @@ Sy.EventDispatcher.EventDispatcher.prototype = Object.create(Sy.EventDispatcher.
                     } else {
                         this.removeListener(
                             name,
-                            subscriber[subscribed[name]]
+                            [subscriber, subscriber[subscribed[name]]]
                         );
                     }
                 }
