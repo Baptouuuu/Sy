2fbd891434b748b890fb8e7b1369c24fa709e33b
Baptiste Langlade <baptouuuu@gmail.com>
2015-01-13 18:37:30 +0100

    update version to 1.2.0

--- dist/storage.js
+++ dist/storage.js
@@ -1,4 +1,4 @@
-/*! sy#1.1.1 - 2014-12-24 */
+/*! sy#1.2.0 - 2015-01-13 */
 /**
  * Transform a dotted string to a multi level object.
  * String like "Foo.Bar.Baz" is like doing window.Foo = {Bar: {Baz: {}}}.
